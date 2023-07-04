0353837030530a575cce3eb221d53735e06f2dde
Arthur <arthurlucas92@orange.fr>
2023-07-04 18:29:13 +0200

    transform all call api into async

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
-function getAllfestivals(){
-    fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&rows=100')
-    .then(response => response.json())
-    .then(data => {
-        // Traitement des données
-        var response = data.records;
-        return response;
-    })
-    .catch(error => {
-        // Gestion des erreurs
+async function getAllfestivals(){
+    try {
+        const response = fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&rows=100')
+        const data = await response.json();
+        const festivalData = data.records;
+        return festivalData;
+    }
+    catch (error) {
         console.error('Une erreur s\'est produite :', error);
-    });
+    } 
 }
 
-function filterFestivalsByQuery(query){
-    fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=' + query)
-    .then(response => response.json())
-    .then(data => {
-        // Traitement des données
-        var response = data.records;
-        return response;
-    })
-    .catch(error => {
-        // Gestion des erreurs
+async function filterFestivalsByQuery(query){
+    try {
+        const response = await fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=' + query)
+        const data = await response.json();
+        const festivalData = data.records;
+        return festivalData;
+    }
+    catch (error) {
         console.error('Une erreur s\'est produite :', error);
-    });
+    } 
+    
+
 }
 
 async function getAllRegions(){
@@ -31,6 +29,7 @@ async function getAllRegions(){
         const response = await fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&facet=region');
         const data = await response.json();
         const festivalData = data.facet_groups[0].facets;
+        console.log(festivalData);
         return festivalData;
       } catch (error) {
         console.error('Une erreur s\'est produite :', error);
