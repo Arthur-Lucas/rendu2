9a01b46fd2a0c623aba066d82e30e92fcbdd6461
Arthur <arthurlucas92@orange.fr>
2023-07-04 15:53:56 +0200

    try to make first api Request

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ function getRandomNumber() {
   return Math.floor(Math.random() * 100);
 }
 
-module.exports = getRandomNumber;
+function getAllfestivals(){
+    fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=&rows=100&facet=region&facet=domaine&facet=complement_domaine&facet=departement&facet=mois_habituel_de_debut')
+    .then(response => response.json())
+    .then(data => {
+        // Traitement des données
+        return data;
+    })
+    .catch(error => {
+        // Gestion des erreurs
+        console.error('Une erreur s\'est produite :', error);
+    });
+}
+
+module.exports.getRandomNumber = getRandomNumber;
+module.exports.getAllfestivals = getAllfestivals;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,17 @@
-const getRandNumber = require('../src/index');
+const myModule  = require('../src/index');
 
 describe('getRandNumber', () => {
     it('should return a random number between 0 and 99', () => {
-      const randomNumber = getRandNumber();
+      const randomNumber = myModule.getRandomNumber();
       expect(typeof randomNumber).toBe('number');
       expect(randomNumber).toBeGreaterThanOrEqual(0);
       expect(randomNumber).toBeLessThan(100);
     });
-  });
+});
+
+describe('getAllfestivals', () => {
+    it('should return an object ', () => {
+        const getAllfestival = myModule.getAllfestivals();
+        console.log(getAllfestival);
+    });
+});
