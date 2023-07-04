69dafb7e30d8b310be5b2967245f1a4ca95f21eb
Arthur <arthurlucas92@orange.fr>
2023-07-04 17:13:44 +0200

    async function ?

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function filterFestivalsByQuery(query){
     .then(data => {
         // Traitement des données
         var response = data.records;
+        console.log(response);
         return response;
     })
     .catch(error => {
@@ -26,18 +27,16 @@ function filterFestivalsByQuery(query){
     });
 }
 
-function getAllRegions(){
-    fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&q=' + query)
-    .then(response => response.json())
-    .then(data => {
-        // Traitement des données
-        var response = data.facet_groups[0].facets;
-        return response;
-    })
-    .catch(error => {
-        // Gestion des erreurs
+async function getAllRegions(){
+    try {
+        const response = await fetch('https://data.culture.gouv.fr/api/records/1.0/search/?dataset=panorama-des-festivals&facet=region');
+        const data = await response.json();
+        const festivalData = data.facet_groups[0].facets;
+        console.log(festivalData);
+        return festivalData;
+      } catch (error) {
         console.error('Une erreur s\'est produite :', error);
-    });
+      }
 }
 
 module.exports = {getAllRegions, getAllfestivals, filterFestivalsByQuery};
